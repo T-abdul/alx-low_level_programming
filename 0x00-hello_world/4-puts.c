d80e1dd924869262bc8d5e36ac7a985d1305ea2d
T-abdul <ibnutanim2@gmail.com>
2022-09-08 14:50:04 -0700

    .

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #!/bin/bash
-#include <stdio.h> 
-/*
- * puts entry
- */
-int main(void)
-{
-	puts("\""Programming is like building a multilingual puzzle");
-	return (0);
-}
+#include <stdio.h> 
+  
+ /** 
+  *   * main - Entry point 
+  *    * Description: this function prints an output 
+  *     * Return: 0 
+  *      */ 
+ int main(void) 
+	 { 
+	          puts("\"Programming is like building a multilingual puzzle"); 
+	          return (0); 
+	  }}
