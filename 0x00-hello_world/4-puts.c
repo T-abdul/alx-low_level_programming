a57998cefcf0dcc4a15f36b128ae5e3273633b62
T-abdul <ibnutanim2@gmail.com>
2022-09-08 15:58:22 -0700

    .

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
-#!/bin/bash
-#include <stdio.h> 
-  
- /** 
-  *   * main - Entry point 
-  *    * Description: this function prints an output 
-  *     * Return: 0 
-  *      */ 
- int main(void) 
-	 { 
-	          puts("\"Programming is like building a multilingual puzzle"); 
-	          return (0); 
-	  }}
+#include <stdio.h>
+
+/**
+* main - print the string in the put funtion
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
