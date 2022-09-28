a49bb8969db752a4e16ed76f0feca1f43f77b688
T-abdul <ibnutanim2@gmail.com>
2022-09-28 08:25:15 -0700

    .

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints a string, followed by a new line.
+ *
+ * @s: string to print
+ *
+ * Return: 0
+ */
+
+void _puts_recursion(char *s);
+{
+	char s[] = "Puts with recursion";
+	{
+		putchar(s);
+	}
+	putchar('\n');
+return (0);
+}
+
